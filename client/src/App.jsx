098e88ec6736ae36cc403de6116c753dfaffc7a9
ValentineOO <varlomxg@gmail.com>
2023-10-28 12:29:22 +0100

    ran

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,32 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { Routes } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import { themeSettings } from "./theme";
+import { Navigate } from "react-router-dom";
+import Layout from "./scenes/layout";
+import Dashboard from "./scenes/dashboard";
 
 const App = () => {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to ="/dashboard" replace />} />
+            </Route>
+            <Route path="/dashboard" element={<Dashboard/>} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
